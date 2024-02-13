113bd20738031c0019895428153abb32ef589fa6
Izabel Jipson <127130244+ijipson@users.noreply.github.com>
2024-02-13 18:05:59 -0500

    Add files via upload

---
+++ webapp/localService/mockserver.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+    "sap/ui/core/util/MockServer"
+], (MockServer) =>{
+    "use strict";
+
+    return{
+        init() {
+            const oMockServer = new MockServer({
+                rootUri: sap.ui.require.toUrl("ui5/walkthrough") + "/V2/Northwind/Northwind.svc/"
+            });
+
+            const oUrlParams = new URLSearchParams(window.location.search);
+
+            MockServer.config({
+                autoRespond: true,
+                autoRespondAfter: oUrlParams.get("serverDelay")
+            });
+            
+            const sPath = sap.ui.require.toUrl("ui5/walkthrough/localService");
+            oMockServer.simulate(sPath + "/metadata.xml", sPath + "/mockdata");
+
+            oMockServer.start();
+        }
+    };
+});
